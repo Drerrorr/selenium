62e2ee6f1401aaf12540d128ac9603ed6b475a63
Drerrorr <137769656+Drerrorr@users.noreply.github.com>
2025-07-07 13:03:30 +0800

    Add files via upload

--- demo3.py
+++ demo3.py
@@ -5,84 +5,53 @@
 # @Software: PyCharm
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import sklearn
-import numpy
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import pyautogui
-import pytesseract
-from PIL import Image
 import requests
 from io import BytesIO
 import time
-import numpy as np
-import os
+import ddddocr
+from PIL import Image
 
 driver = webdriver.Chrome()
-driver.get('https://exam.edu.foshan.gov.cn/iexamfs/KsLoginSuccessAction.action')
+driver.get('https://exam.edu.foshan.gov.cn:49000/iexam/iexam/basis/KsLoginAction/toLogin.do')
 time.sleep(1)
-pyautogui.keyDown('Enter')# 模拟按下回车键
 cookies = driver.get_cookies()#获取所有cookie
 cookie_value = next((cookie['value'] for cookie in cookies if cookie['name'] == 'SESSION'), None)#筛选出SESSION的值
 print(cookie_value)
-src = driver.find_element(By.XPATH,'/html/body/form/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td/table/tbody/tr[3]/td[2]/img').get_attribute('src')#获取src
-print(src)
-captcha_url = src    #获取图片的地址（src）
-url = src
+src = driver.find_element(By.XPATH,'/html/body/div/table/tbody/tr[2]/td/form/div/div[2]/div[3]/img').get_attribute('src')#获取src
+print(src)#获取图片的地址（src）
+url=src
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-    'Content-Type': 'application/json',
-    'Accept' :'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
-    'Host': 'exam.edu.foshan.gov.cn',
-    'Sec-Fetch-platform':'"Android"',
-    'Sec-Fetch-Mode':'no-cors',
-    'Sec-Fetch-Dest':'image',
-    'Referer':'https://exam.edu.foshan.gov.cn/iexamfs/KsLoginSuccessAction.action',
+    'Accept':'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
     'Accept-Encoding':'gzip, deflate, br, zstd',
-    'Accept-Language':'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Cookie': 'SESSION=%s; name=value' % cookie_value
+    'Accept-Language':'zh-CN,zh;q=0.9',
+    'Connection':'keep-alive',
+    'Cookie':'SESSION=%s;common-secure' % cookie_value ,
+    'Host':'exam.edu.foshan.gov.cn:49000',
+    'Referer':'https://exam.edu.foshan.gov.cn:49000/iexam/iexam/basis/KsLoginAction/toLogin.do',
+    'Sec-Fetch-Dest':'image',
+    'Sec-Fetch-Mode':'no-cors',
+    'Sec-Fetch-Site':'same-origin',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
+    'sec-ch-ua':'"Google Chrome";v="137", "Chromium";v="137", "Not/A)Brand";v="24"',
+    'sec-ch-ua-mobile':'?0',
+    'sec-ch-ua-platform':'"Windows"'
+
 
 }
 response = requests.get(url,headers=headers)#使用特定headers请求
 image_stream = BytesIO(response.content)
 print(response.content)
 img = Image.open(image_stream) # 使用Pillow打开图片
-img.save('L:\\pycham\\pycharm demo\\demo2\\pic\\serialno.bmp', 'BMP')# 保存图片为BMP格式
-image = Image.open('L:\\pycham\\pycharm demo\\demo2\\pic\\serialno.bmp')
-image = image.convert('L')#将图片转换为灰度图像
-image = np.asarray(image)#将图片转换为numpy数组
-print(image.shape)
-image = (image > 135) * 255
-split_parts = [
-    [36, 46],
-    [51, 61],
-    [68, 78],
-    [84, 94]
-]
-letters = []
-save_folder = 'L:\\pycham\\pycharm demo\\demo2\\pic1'
-for part in split_parts:
-    letter = image[1:10, part[0]:part[1]]
-    letters.append(letter.reshape(10*24))
+img.save('C:\\Users\\ASUS\\Desktop\\demo2\\pic\\randCode.jpg')# 保存图片为jpg格式
 
-'''
-def load_dataset():
-    X = []
-    y = []
 
-    for i in range(70):
-        path = "./train/%d%d.bmp" % (i / 7, i % 7)
-        pix = np.asarray(Image.open(path).convert("L"))
-        X.append(pix.reshape(10*23))
-        y.append(int(i / 7))
-    return np.asarray(X), np.asarray(y)
-
-from sklearn.neighbors import KNeighborsClassifier
-
-knn = KNeighborsClassifier()
-knn.fit(X, y)
-knn.predict(letters)
-'''
+ocr = ddddocr.DdddOcr(beta=True)  # 切换为第二套ocr模型
+image = open('C:\\Users\\ASUS\\Desktop\\demo2\\pic\\randCode.jpg', "rb").read()
+result = ocr.classification(image)
+print(result)
+time.sleep(1000000)
 '''
 #将目标定向在账号框
 driver.find_element(By.XPATH,'/html/body/form/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td/table/tbody/tr[1]/td[2]/input').send_keys('123456789')
